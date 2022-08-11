d3ccac561ba574cedaa067ea546865d0feb91455
Roy Moore <roy@moore.co.il>
2022-08-11 10:17:11 +0300

    Refactor books data source

--- books/books_data.js
+++ books/books_data.js
@@ -6,4 +6,30 @@ let books_json = '{ "books" : [' +
     '{ "title":"The Collected Stories of Arthur C. Clarke" , "author":"ACC" , "cover":"https://upload.wikimedia.org/wikipedia/en/d/d7/Collected_stories_clarke.jpg"},' +
     '{ "title":"Ender\'s Game" , "author":"OSC" , "cover":"https://upload.wikimedia.org/wikipedia/en/e/e4/Ender%27s_game_cover_ISBN_0312932081.jpg"} ]}';
 
-const books_data = JSON.parse(books_json);
+function populte_books(placeholder) {
+    books_data = JSON.parse(books_json);
+
+    Object.values(books_data.books).forEach(val => {
+        $(placeholder).before(
+            `<div class="book">
+               <div class="side spine">
+                   <span class="spine-title"> ${val.title} </span>
+                   <span class="spine-author"> ${val.author} </span>
+               </div>
+               <div class="side top"></div>
+               <div class="side cover"
+                   style="background-image: url(&quot;${val.cover}&quot;); height: 274px; top: 6px;">
+               </div>
+          </div>`);
+    });
+}
+
+/* Usage:
+    <body>
+        <books></books>
+    </body>
+
+    <script>
+        populte_books("books")
+    </script>
+*/
